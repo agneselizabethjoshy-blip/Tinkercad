41585147ab117c9807bb11c89376719eab86fe43
agneselizabethjoshy-blip <agneselizabethjoshy@gmail.com>
2026-07-30 12:54:46 +0530

    Create sketch.ino

---
+++ automatic street light/sketch.ino
@@ -0,0 +1,24 @@
+int ldrPin = A0;
+int ledPin = 8;
+
+int lightValue;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  lightValue = analogRead(ldrPin);
+
+  Serial.println(lightValue);
+
+  if (lightValue < 500) {   // Dark
+    digitalWrite(ledPin, HIGH);
+  } 
+  else {                    // Bright
+    digitalWrite(ledPin, LOW);
+  }
+
+  delay(200);
+}
